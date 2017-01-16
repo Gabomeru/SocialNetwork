edcd317c5dcfbb21505953d3f111f896d32fc54e
Gabomeru <gabo.meruvia@gmail.com>
2017-01-15 21:50:57 -0400

    feed vc part 1 finished

---
+++ SocialNetwork/CircleView.swift
@@ -0,0 +1,29 @@
+//
+//  CircleView.swift
+//  SocialNetwork
+//
+//  Created by Gabriel Meruvia on 1/15/17.
+//  Copyright © 2017 Gabriel Meruvia. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+    }
+        
+    
+}
